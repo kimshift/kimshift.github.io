fa9163013d987103c184ebad1ee68e2f22faa25a
chenzhifeng <chenzhifeng@ly-sky.com>
2024-07-24 17:46:04 +0800

    修改配置文件

--- .vitepress/config/giscus.js
+++ .vitepress/config/giscus.js
@@ -6,10 +6,10 @@
 
 // giscus评论配置官网地址：https://giscus.app/zh-CN
 export default {
-  repo: 'kimshift/giscus-dev', // 存储评论的<用户名/仓库名>
-  repoId: 'R_kgDOMJLKTw', // 存储评论的仓库id
+  repo: 'kimshift/kimshift.github.io', // 存储评论的<用户名/仓库名>
+  repoId: 'R_kgDOL0rB9A', // 存储评论的仓库id
   category: 'Announcements', // 分类
-  categoryId: 'DIC_kwDOMJLKT84CgE88', // 分类id
+  categoryId: 'DIC_kwDOL0rB9M4ChIGT', // 分类id
   loading: 'lazy', // 懒加载评论
   inputPosition: 'top', // 输入框位置
 }
